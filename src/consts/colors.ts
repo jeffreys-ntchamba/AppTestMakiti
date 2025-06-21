1ebc1b617712b55a58f4b97e0031c068b184c30e
jeff <jeff>
2025-06-21 02:04:04 +0200

    added const (colors and texts)

---
+++ src/consts/colors.ts
@@ -0,0 +1,9 @@
+export default {
+    primary: '#F0D2A9',
+    gray: "#f4f4f4",
+    textMuted: "#0C0C0C66",
+    black: "#171C26",
+  };
+  
+
+  
---
+++ src/consts/texts.ts
@@ -0,0 +1,21 @@
+export default {
+    title: "Pâte à tartiner Nutella 1Kg",
+    location: "Lagos",
+    productSizeLabel: "Product size",
+    productSizeValue: "1 kg",
+    productPriceLabel: "Product price",
+    productPriceValue: "XAF 5,500",
+    orderLabel: "Order",
+    orderValue: "2",
+    totalPriceLabel: "Total price",
+    totalPriceValue: "XAF 11,000",
+    buttonLabel: "Add Order to Basket",
+    bottomNav: {
+      products: "Products",
+      sales: "Sales",
+      scan: "Scan",
+      delivery: "Delivery",
+      invoices: "Invoices",
+    },
+  };
+  
